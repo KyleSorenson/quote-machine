c8646d7a09f8dfc71739107f4dd6697606fd3c25
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-18 17:32:00 -0700

    Completed Version 1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
+import React from 'react';
 import './App.css';
 
-const QUOTES = [
-  { 
-    quote: "I Came. I Saw. I Conquered.",
-    author: "Julius Caesar" 
-  },
-  { 
-    quote: "To be or not to be? That is the question.",
-    author: "William Shakespeare" 
-  },
-]
+const QUOTES = require('./quotes.json');
 
-function QuoteBox(props) {
-  return(
-    <div id="quote-box">
-      <div id="text"><h1>"{props.quote}"</h1></div>
-      <div id="author">~{props.author}</div>
-      <button id="new-quote">New Quote</button>
-      <a id="tweet-quote" href="kylesorensonmusic.com">Tweet Quote</a>
-    </div>
-  );
-}
+class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteIndex: 0
+    }
+    this.newQuote = this.newQuote.bind(this);
+  }
 
+  newQuote = () => {
+    this.setState((state) => ({
+      quoteIndex: Math.floor(Math.random() * (QUOTES.length))
+    }));
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <div id="text"><h1 id="quote">{QUOTES[this.state.quoteIndex].quote}</h1></div>
+        <div id="author">{QUOTES[this.state.quoteIndex].author}</div>
+        <div id="links">
+          <button
+            id="new-quote"
+            onClick={this.newQuote}>
+              New Quote
+          </button>
+          <a
+            id="tweet-quote"
+            href={
+              "http://twitter.com/intent/tweet?text=" + 
+              QUOTES[this.state.quoteIndex].quote + 
+              ' -' + 
+              QUOTES[this.state.quoteIndex].author
+            }
+            target="_blank">
+              Tweet Quote
+          </a>
+        </div>
+      </div>
+    );
+  }
+};
 
 function App() {
   return (
     <div id="container">
-      <QuoteBox 
-        quote={QUOTES[0].quote}
-        author={QUOTES[0].author}/>
-    </div>  
+      <QuoteBox />
+    </div>
   );
 }
 
 export default App;
+
+
+//https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Eighty%20percent%20of%20success%20is%20showing%20up.%22%20Woody%20Allen
